9b1d96097bd3f8bb35473d5905b9d1574d3627b1
Ignas Mikalajunas <ignas.mikalajunas@cloudkitchens.com>
2021-01-03 08:03:53 +0200

    No more deploys from my personal machine, only serve while working

--- serve.sh
+++ serve.sh
@@ -4,6 +4,4 @@ JEKYLL_VERSION=4.1.0
 docker run --rm -p 4000:4000 \
   --volume="$PWD:/srv/jekyll" \
   -it jekyll/builder:$JEKYLL_VERSION \
-  jekyll build
-gcloud config set account ignas.mikalajunas@gmail.com
-gsutil -m rsync -d -r ./_site gs://pow.lt
+  jekyll serve --incremental
