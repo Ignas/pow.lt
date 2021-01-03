5b43b0cbf98d422c0eb03f9f34b6b73e96125ca7
Ignas Mikalajunas <ignas.mikalajunas@cloudkitchens.com>
2021-01-03 03:04:56 +0200

    Set account before uploading

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,5 @@ docker run --rm -p 4000:4000 \
   --volume="$PWD:/srv/jekyll" \
   -it jekyll/builder:$JEKYLL_VERSION \
   jekyll build
+gcloud config set account ignas.mikalajunas@gmail.com
 gsutil -m rsync -d -r ./_site gs://pow.lt
